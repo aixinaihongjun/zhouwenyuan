c92df0072ab3094cc7ea9c61007c1b884ae0485c
aixinaihongjun <1181391196@qq.com>
2021-12-24 22:48:57 +0800

    主界面

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>引文管理系统</h1>
+    <div class="search">
+      <el-input placeholder="请输入文章ID" v-model="input" clearable> </el-input>
+      <el-row>
+        <el-button type="primary">搜索</el-button>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      input: "",
+    };
+  },
+};
+</script>
+<style>
+.search {
+  display: flex;
+  width: 80%;
+  margin: auto;
+}
+</style>
